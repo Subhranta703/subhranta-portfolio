77da5a7d82e314753e189968164c9e30bde9fd24
subhranta kumar nayak <114854318+Subhranta703@users.noreply.github.com>
2024-05-04 02:57:16 +0530

    First

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,47 @@
+import "./intro.css";
+
+import { FaAward } from "react-icons/fa";
+import React from "react";
+import { VscFolderLibrary } from "react-icons/vsc";
+import img from '../../assets/Me.png'
+
+
+
+
+const Intro = () => {
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={img} alt="subhranta kumar nayak" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>6Months</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>5+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+          6+ months experienced Front End Developer skilled in designing and implementing responsive user interfaces using React concepts. Proficient in translating designs and wireframes into high-quality code. Experienced in writing application interface code in JavaScript and React JS workflows, monitoring and maintaining frontend performance, and troubleshooting and debugging to enhance overall performance.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
